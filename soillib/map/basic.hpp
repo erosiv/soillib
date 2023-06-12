07b10e817a4b7ccc0d7d3d8fb474e8ffe8eb122b
Nicholas McDonald <nick@Nicks-MBP.home>
2023-06-12 17:18:33 +0200

    added scale to the map. this makes sense for abstraction purposes

--- soillib/map/basic.hpp
+++ soillib/map/basic.hpp
@@ -4,26 +4,36 @@
 #include <soillib/soillib.hpp>
 #include <soillib/util/index.hpp>
 #include <soillib/util/slice.hpp>
-#include <soillib/util/pool.hpp>
 
 namespace soil {
 namespace map {
+
+struct basic_config {
+  glm::ivec2 dimension = glm::ivec2(0);
+  float scale = 0.0f;
+};
 
 template<typename T, soil::index_t Index> struct basic_iterator;
 template<typename T, soil::index_t Index = soil::index::flat>
 struct basic {
 
   typedef Index index;
+  typedef basic_config config;
 
   const glm::ivec2 dimension;
+  const float scale;
+
   const size_t area = dimension.x*dimension.y;
 
   soil::slice<T, Index> slice;
 
-  basic(const glm::ivec2 dimension):dimension(dimension){}
-  basic(const glm::ivec2 dimension, soil::pool<T>& pool):dimension(dimension){
-    slice = {pool.get(area), dimension};
-  }
+  basic(const glm::ivec2 dimension, const float scale):
+    dimension(dimension),
+    scale(scale)
+  {}
+  basic(const config config):
+    basic(config.dimension, config.scale)
+  {}
 
   inline T* get(const glm::ivec2 p) noexcept {
     return slice.get(p);
@@ -66,6 +76,23 @@ struct basic_iterator {
   };
 
 };
+
+// Configuration Loading
+
+#ifdef SOILLIB_IO_YAML
+
+bool operator<<(basic_config& conf, soil::io::yaml::node& node){
+  try {
+    conf.dimension.x = node["dimension"][0].As<int>();
+    conf.dimension.y = node["dimension"][1].As<int>();
+    conf.scale = node["scale"].As<float>();
+  } catch(soil::io::yaml::exception& e){
+    return false;
+  }
+  return true;
+}
+
+#endif
 
 }; // namespace map
 }; // namespace soil
