d6d594e6e614faeb75f0dd95df667965f1f2c9cb
weigert <nicholasmcdonald40@gmail.com>
2023-12-21 23:34:03 +0100

    working normalmap

--- tools/view/main.cpp
+++ tools/view/main.cpp
@@ -13,8 +13,8 @@
 
 struct cell {
   float height;
-  float discharge;
-  glm::vec4 normal;
+ // float discharge;
+  glm::vec3 normal;
 };
 
 int main( int argc, char* args[] ) {
@@ -28,9 +28,9 @@ int main( int argc, char* args[] ) {
   // Load Image Data
 
   soil::io::tiff height((path + "/height.tiff").c_str());
-  soil::io::tiff discharge((path + "/discharge.tiff").c_str());
+  //soil::io::tiff discharge((path + "/discharge.tiff").c_str());
   soil::io::png normal((path + "/normal.png").c_str());
-  soil::io::png albedo((path + "/albedo.png").c_str());
+  //soil::io::png albedo((path + "/albedo.png").c_str());
 
   // Create Map
 
@@ -40,20 +40,25 @@ int main( int argc, char* args[] ) {
   // Fill Map
 
   for(auto [cell, pos]: map){
-    cell.height = 80.0f*height[pos];
-    cell.discharge = discharge[pos];
-    cell.normal = glm::vec4(normal[pos]);
+    cell.height = height[pos];
+    //cell.discharge = discharge[pos];
+    glm::vec4 _normal = normal[pos];
+    _normal /= 255.0f;
+    _normal = 2.0f*_normal - 1.0f;
+    //_normal[1] *= -1.0f;//*= -1.0f;
+    cell.normal = glm::normalize(glm::vec3(_normal));
   }
 
 	Tiny::view.vsync = false;
-	Tiny::window("soillib dataset viewer", 1200, 800);			//Open Window
+	Tiny::window("soillib dataset viewer", 1200, 1000);			//Open Window
 
-	cam::near = -500.0f;
-	cam::far = 500.0f;
-	cam::rot = 45.0f;
+	cam::near = -1000.0f;
+	cam::far = 1000.0f;
+	cam::rot = 0.0f;
 	cam::roty = 45.0f;
-  cam::turnrate = 0.1f;
-	cam::init(10, cam::ORTHO);
+  cam::turnrate = 1.0f;
+  cam::zoomrate *= 0.2f;
+	cam::init(7, cam::ORTHO);
 	cam::update();
 
 	Tiny::event.handler = cam::handler;								//Event Handler
@@ -65,23 +70,36 @@ int main( int argc, char* args[] ) {
 	Model mesh({"in_Position"});					//Create Model with 2 Properties
 	mesh.bind<glm::vec3>("in_Position", &positions);	//Bind Buffer to Property
 	mesh.index(&indices);
-	mesh.model = glm::translate(glm::mat4(1.0f), glm::vec3(-map.dimension.x/2, -15.0, -map.dimension.y/2));
-
+  mesh.model = glm::scale(glm::mat4(1.0f), glm::vec3(0.1));
+	mesh.model = glm::translate(mesh.model, glm::vec3(-map.dimension.x/2, -15.0, -map.dimension.y/2));
 	Shader defaultShader({"shader/default.vs", "shader/default.fs"}, {"in_Position"});
 
   // Textures
 
-  Texture dischargeMap(image::make([&](const glm::ivec2 p){
-    return glm::vec4(discharge[p]);
-  }, map.dimension));
+ // Texture dischargeMap(image::make([&](const glm::ivec2 p){
+ //   return glm::vec4(discharge[p]);
+ // }, map.dimension));
 
-  Texture normalMap(image::make([&](const glm::ivec2 p){
-    return glm::vec4(normal[p]);
-  }, map.dimension));
+  Texture normalMap(map.dimension.x, map.dimension.y, {GL_RGBA32F, GL_RGBA, GL_FLOAT});
+  
+  auto data = new glm::vec4[map.dimension.x*map.dimension.y];
+  for(size_t x = 0; x < map.dimension.x; x++)
+  for(size_t y = 0; y < map.dimension.y; y++){
+    data[y*map.dimension.x + x] = glm::vec4(map.get(glm::ivec2(x, y))->normal, 0.0f);
+  }
 
-  Texture albedoMap(image::make([&](const glm::ivec2 p){
-    return glm::vec4(albedo[p]);
-  }, map.dimension));
+  glBindTexture(GL_TEXTURE_2D, normalMap.texture);
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, map.dimension.x, map.dimension.y, 0, GL_RGBA, GL_FLOAT, data);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glBindTexture(GL_TEXTURE_2D, 0);
+  delete[] data;
+
+  //Texture albedoMap(image::make([&](const glm::ivec2 p){
+  //  return glm::vec4(albedo[p]);
+  //}, map.dimension));
 
 	Tiny::view.pipeline = [&](){											//Setup Drawing Pipeline
 
@@ -90,9 +108,9 @@ int main( int argc, char* args[] ) {
 		defaultShader.use();														//Prepare Shader
 		defaultShader.uniform("model", mesh.model);			//Set Model Matrix
     defaultShader.uniform("vp", cam::vp);						//View Projection Matrix
-    defaultShader.texture("dischargeMap", dischargeMap);						//View Projection Matrix
+  //  defaultShader.texture("dischargeMap", dischargeMap);						//View Projection Matrix
     defaultShader.texture("normalMap", normalMap);            //View Projection Matrix
-    defaultShader.texture("albedoMap", albedoMap);            //View Projection Matrix
+  //  defaultShader.texture("albedoMap", albedoMap);            //View Projection Matrix
     defaultShader.uniform("dimension", glm::vec2(map.dimension));						//View Projection Matrix
 		mesh.render(GL_TRIANGLES);													//Render Model with Lines
 
--- tools/view/shader/default.vs
+++ tools/view/shader/default.vs
@@ -5,9 +5,9 @@ in vec3 in_Position;
 uniform mat4 model;
 uniform mat4 vp;
 
-uniform sampler2D dischargeMap;
+//uniform sampler2D dischargeMap;
 uniform sampler2D normalMap;
-uniform sampler2D albedoMap;
+//uniform sampler2D albedoMap;
 uniform vec2 dimension;
 
 out vec4 ex_Color;
@@ -15,26 +15,17 @@ out vec3 ex_Normal;
 out vec3 ex_FragPos;
 
 void main(void) {
+
 	//Fragment Position in Model Space
 	ex_FragPos = (model * vec4(in_Position, 1.0f)).xyz;
-	ex_Normal = texture(normalMap, in_Position.xz/dimension).xyz;
-	vec3 ex_Albedo = 1.0f-texture(albedoMap, in_Position.xz/dimension).xyz;
-
-	//Fragment in Screen Space
-	gl_Position = vp * vec4(ex_FragPos, 1.0f);
-
-	float discharge = texture(dischargeMap, in_Position.xz/dimension).a;
-
-	vec3 normal = 1.0f-ex_Normal*2.0f;
-
-	float diffuse = dot(normalize(normal), normalize(vec3(1, 1, 1)));
-	diffuse = clamp(diffuse, 0.1, 0.9);
+	gl_Position = vp * vec4(ex_FragPos, 1.0f); // Screen Space
 	
-	float light = 0.7 + 0.5*diffuse;
+	vec3 ex_Normal = texture(normalMap, in_Position.xz/dimension).xyz;
+	float diffuse = dot(normalize(ex_Normal), normalize(vec3(0, 0, 1)));
+	diffuse = clamp(diffuse, 0.05, 0.95);
 
-	ex_Normal = 1.0f-ex_Normal.xyz;
-	ex_Color = vec4(vec3(light), 1.0);
+	float light = 0.4 + diffuse;
+	
+	vec3 ex_Albedo = vec3(1);
 	ex_Color = vec4(light*ex_Albedo, 1.0f);
-	ex_Color = vec4(ex_Normal, 1.0f);
-	ex_Color = mix(ex_Color, vec4(1,1,1,1), 0.6*discharge);
 }
