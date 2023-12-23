2c8e1523103fe0a51566756c806418c20b4b43f0
weigert <nicholasmcdonald40@gmail.com>
2023-12-23 16:32:50 +0100

    Added an isoline export to the tif_relief

--- tools/tif_relief/main.cpp
+++ tools/tif_relief/main.cpp
@@ -46,7 +46,9 @@ int main(int argc, char *args[]) {
 
   // Load the Image First
 
-  geovalue_t height(path.c_str());
+  geovalue_t height;
+  height.meta(path.c_str());
+  height.read(path.c_str());
 
   // Create Cell Pool, Map
 
@@ -108,5 +110,38 @@ int main(int argc, char *args[]) {
     return 255.0f*glm::vec4(d*color, 1.0f);
   });
   image.write("relief.png");
+
+  const auto iso_export = [&](const float scale){
+
+    soil::io::tiff<float> isoheight(height.width, height.height);
+    soil::io::png isoline(height.width, height.height);
+
+    isoheight.fill([&](const glm::ivec2 pos){
+      float h = world.map.get(pos)->height;
+      return float((int)( h * scale)) / scale;
+    });
+
+    isoline.fill([&](const glm::ivec2 pos){
+
+      if(!world.map.oob(pos + glm::ivec2(1, 1))){  
+        float h00 = isoheight[pos + glm::ivec2(0, 0)];
+        float h01 = isoheight[pos + glm::ivec2(0, 1)];
+        float h10 = isoheight[pos + glm::ivec2(1, 0)];
+        float h11 = isoheight[pos + glm::ivec2(1, 1)];
+        if(h00 != h01 || h00 != h10 || h00 != h11){
+          return glm::vec4(255, 255, 255, 255);
+        }
+      }
+      return glm::vec4(0, 0, 0, 255);
+    });
+
+    isoline.write(("isoline_"+std::to_string((int)scale)+".png").c_str());
+
+  };
+
+ // iso_export(5.0f);
+ // iso_export(10.0f);
+ // iso_export(25.0f);
+ // iso_export(50.0f);
 
 }
