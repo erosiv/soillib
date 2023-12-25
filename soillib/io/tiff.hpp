763f8dc5df7b2ff7384f475fd70ff12019041391
weigert <nicholasmcdonald40@gmail.com>
2023-12-25 16:15:48 +0100

    Set of tweaks to the io section of the code, to make geotiffs utilize more common behavior and enable tiled tiffs in general

--- soillib/io/tiff.hpp
+++ soillib/io/tiff.hpp
@@ -7,53 +7,122 @@
 namespace soil {
 namespace io {
 
-// TIFF Implementation
-
+//! Strong-Typed TIFF Implementation
 template<typename T = float>
 struct tiff: soil::io::img<T> {
 
+  using soil::io::img<T>::value_t;
+  using soil::io::img<T>::img;
   using soil::io::img<T>::allocate;
   using soil::io::img<T>::operator[];
 
-  tiff(){};
   tiff(const char* filename){ read(filename); };
-  tiff(const size_t width, const size_t height):img<T>(width, height){}
-  tiff(const glm::ivec2 res):img<T>(res){}
 
-  bool read(const char* filename);
-  bool write(const char* filename);
+  bool meta(const char* filename);  //!< Load TIFF Metadata
+  bool read(const char* filename);  //!< Read TIFF Raw Data
+  bool write(const char* filename); //!< Write TIFF Raw Data
 
+private:
+  bool meta_loaded = false;
+  bool tiled_image = false;
+  uint32_t twidth = 0;
+  uint32_t theight = 0;
 };
 
-// Implementations
-
+//! Load TIFF Metadata
 template<typename T>
-bool tiff<T>::read(const char* filename){
+bool tiff<T>::meta(const char* filename){
 
   TIFF* tif = TIFFOpen(filename, "r");
 
-  uint32_t width;
-  uint32_t height;
+  // Meta-Data
 
-  TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &width);
-  TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &height);
+  if(!TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &this->width))
+    return false;
 
-  if(width != this->width || height != this->height || this->data == NULL){
-    this->width = width;
-    this->height = height;
-    if(this->data != NULL){
-      delete[] this->data;
-      this->data = NULL;
+  if(!TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &this->height))
+    return false;
+
+  if(TIFFGetField(tif, TIFFTAG_TILEWIDTH, &this->twidth))
+    this->tiled_image = true;
+
+  if(TIFFGetField(tif, TIFFTAG_TILELENGTH, &this->theight))
+    this->tiled_image = true;
+
+  TIFFClose(tif);
+
+  this->meta_loaded = true;
+  return true;
+
+}
+
+//! Read TIFF Raw Data
+template<typename T>
+bool tiff<T>::read(const char* filename){
+
+  if(!meta_loaded){
+    this->meta(filename);
+  }
+
+  TIFF* tif = TIFFOpen(filename, "r");
+
+  if(this->data != NULL){
+    delete[] this->data;
+    this->data = NULL;
+  }
+  allocate();
+
+  // Load Tiled / Non-Tiled Images
+
+  if(!this->tiled_image){
+
+    T* buf = this->data;
+    for (size_t row = 0; row < this->height; row++){
+      TIFFReadScanline(tif, buf, row);
+      buf += this->width;
     }
-    allocate();
+
+  } else {
+
+    const size_t tsize = this->twidth * this->theight;
+    const size_t nwidth = (this->width + this->twidth - 1)/this->twidth;
+    const size_t nheight = (this->height + this->theight - 1)/this->theight;
+
+    T* buf = this->data;
+    T* nbuf = new T[tsize];
+
+    for(size_t nx = 0; nx < nwidth; ++nx)
+    for(size_t ny = 0; ny < nheight; ++ny){
+
+      const glm::ivec2 npos(nx, ny);
+      const glm::ivec2 norg = npos*glm::ivec2(this->twidth, this->theight);
+
+      if(!TIFFReadTile(tif, nbuf, norg.x, norg.y, 0, 0)){
+        continue;
+      }
+
+      for(size_t ix = 0; ix < this->twidth; ix++)
+      for(size_t iy = 0; iy < this->theight; iy++){
+
+        glm::ivec2 tpos = glm::ivec2(ix, iy);
+        glm::ivec2 fpos = norg + tpos;
+
+       if(fpos.x >= this->width) continue;
+       if(fpos.y >= this->height) continue;
+
+        if(buf[fpos.y * this->width + fpos.x] == 0){
+
+          buf[fpos.y * this->width + fpos.x] = nbuf[iy * this->twidth + ix];
+        }
+
+      }
+    }
+
+    delete[] nbuf;
+
   }
 
-  T* buf = this->data;
-  for (size_t row = 0; row < this->height; row++){
-    TIFFReadScanline(tif, buf, row);
-    buf += this->width;
-  }
-
+  TIFFClose(tif);
   return true;
 
 }
@@ -62,28 +131,27 @@ template<typename T>
 bool tiff<T>::write(const char *filename) {
   
   TIFF *out= TIFFOpen(filename, "w");
+
   TIFFSetField(out, TIFFTAG_IMAGEWIDTH, this->width);
   TIFFSetField(out, TIFFTAG_IMAGELENGTH, this->height);
-  TIFFSetField(out, TIFFTAG_SAMPLESPERPIXEL, 1);   // set number of channels per pixel
-  TIFFSetField(out, TIFFTAG_BITSPERSAMPLE, 8*sizeof(T));    // set the size of the channels
-  TIFFSetField(out, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);    // set the origin of the image.
-  //   Some other essential fields to set that you do not have to understand for now.
+  TIFFSetField(out, TIFFTAG_SAMPLESPERPIXEL, 1);
+  TIFFSetField(out, TIFFTAG_BITSPERSAMPLE, 8*sizeof(T));
+  TIFFSetField(out, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);
   TIFFSetField(out, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
   TIFFSetField(out, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_MINISBLACK);
   TIFFSetField(out, TIFFTAG_SAMPLEFORMAT, SAMPLEFORMAT_IEEEFP);
-  // We set the strip size of the file to be size of one row of pixels
   TIFFSetField(out, TIFFTAG_ROWSPERSTRIP, TIFFDefaultStripSize(out, this->width));
 
   T* buf = this->data;
-  //Now writing image to the file one strip at a time
   for (uint32_t row = 0; row < this->height; row++) {
-      if (TIFFWriteScanline(out, buf, row, 0) < 0)
+    if (TIFFWriteScanline(out, buf, row, 0) < 0)
       break;
     buf += this->width;
   }
 
   TIFFClose(out);
   return true; 
+
 }
 
 }; // end of namespace io
