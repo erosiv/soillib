2f8d5b2d8c092360aece86e9bef42590a0e73edf
weigert <nicholasmcdonald40@gmail.com>
2024-10-28 19:16:52 +0100

    functional path descent but the maps are not idealized yet, so they get stuck in certain locations. local averaging first, or blurring of some kind, would likely help with this problem. otherwise you would have to do the basin filling thing effectively, which could also be implemented no problem in principle

--- example/path_integral/path_catchment.py
+++ example/path_integral/path_catchment.py
@@ -14,10 +14,13 @@ import soillib as soil
 import matplotlib.pyplot as plt
 import numpy as np
 
+from scipy.ndimage import gaussian_filter
+
 def calc_d8(data):
 
   # De-Normalize
   data = 2.0*data - 1.0
+  data[:,:,1] *= -1
   direction = data[:,:,:2]
 
   # Get 2D Normal Vectors
@@ -44,8 +47,10 @@ def calc_d8(data):
 
 def main():
 
-  input = "/home/nickmcdonald/Datasets/UpperAustriaDGM/40718_DGM_tif_Traunkirchen/G-T4831-72.tif"
+  #input = "/home/nickmcdonald/Datasets/UpperAustriaDGM/40718_DGM_tif_Traunkirchen/G-T4831-72.tif"
   #input = "/home/nickmcdonald/Datasets/UpperAustriaDGM/40718_DGM_tif_Traunkirchen/G-T4831-79.tif"
+  #input = "G-T4831-52_BLURRED.tif"
+  input = "/home/nickmcdonald/Datasets/UpperAustriaDGM/40701_DGM_tif_Altmuenster/G-T4831-52.tif"
   image = soil.tiff(input)
   node = image.node()
 
@@ -61,8 +66,9 @@ def main():
   # data[:,:,0] = grid[0]
   # data[:,:,1] = grid[1]
 
-  new_data = calc_d8(data)
-  new_data = np.transpose(new_data, (1,0))
+  dir_d8 = calc_d8(data).astype(np.int64)
+
+  # print(p)
 
   # Generate a Set of Paths...?
   # 1. Seed Random Points
@@ -71,8 +77,41 @@ def main():
   # 4. See if we can visualize these paths somehow??
   # 5. That would basically confirm that we are doing it right.
 
-  plt.imshow(new_data)
+  # Get original position...
+  
+  shape = dir_d8.shape
+  samples = 2048
+
+  pos = np.random.rand(samples, 2, 1)
+  pos[:,0,:] *= shape[0]
+  pos[:,1,:] *= shape[1]
+  pos = pos.astype(np.int64)
+
+  for n in range(2048):
+    # compute the next position
+    npos = pos[..., -1].astype(np.int64)
+    direction = dir_d8[npos[:,0], npos[:,1]]
+
+    # For the Gaussian:
+    npos[direction == 0] += 2 * np.array([ 1, 0])
+    npos[direction == 1] += 2 * np.array([ 1, 1])
+    npos[direction == 2] += 2 * np.array([ 0, 1])
+    npos[direction == 3] += 2 * np.array([-1, 1])
+    npos[direction == 4] += 2 * np.array([-1, 0])
+    npos[direction == 5] += 2 * np.array([-1,-1])
+    npos[direction == 6] += 2 * np.array([ 0,-1])
+    npos[direction == 7] += 2 * np.array([ 1,-1])
+
+    npos = np.clip(npos, [0,0], [shape[0]-1,shape[1]-1])
+    pos = np.append(pos, npos[..., np.newaxis], axis=-1)
+
+  plt.imshow(dir_d8)
   plt.colorbar()
+
+  #plt.scatter(pos[:,1,0], pos[:,0,0], color='w')
+  for n in range(samples):
+    plt.plot(pos[n,1,:], pos[n,0,:], color='black')
+
   plt.show()
 
 if __name__ == "__main__":
