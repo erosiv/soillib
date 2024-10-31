d621979ab7796e5a8e2687153c6012f7ccb97588
weigert <nicholasmcdonald40@gmail.com>
2024-10-31 17:37:49 +0100

    functioning area weighted mutations. now just needs to be sampled stochastically and performance improved

--- example/markov_condition.py
+++ example/markov_condition.py
@@ -4,6 +4,7 @@ from pysheds.grid import Grid
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
+from tqdm import tqdm
 
 '''
 Markov-Chain Monte-Carlo DEM Conditioning
@@ -18,9 +19,16 @@ Primary Problem:
 3. Directed Conditioning of Height
 '''
 
+# Flow Direction Set and Lookup
+
 '''
-dirmap = (6, 7, 0, 1, 2, 3, 4, 5)
-dirset = [
+ 6  7  8
+ 5  0  1
+ 4  3  2
+'''
+
+dirmap = (7, 8, 1, 2, 3, 4, 5, 6)
+dirset = np.array([
   np.array([ 0, 1]),
   np.array([ 1, 1]),
   np.array([ 1, 0]),
@@ -29,27 +37,69 @@ dirset = [
   np.array([-1,-1]),
   np.array([-1, 0]),
   np.array([-1, 1])
-]
+])
 
-def direction(code):
-  return dirset[code]
+'''
+Energy Function:
+Option 1:
+-> Simple Neighborhood Majority
+
+Option 2:
+-> Incoming Vector Weighting
+
+Option 3:
+-> Something w. Divergence
+-> Divergence can't vanish!
 '''
 
-def compute_index(dem, flow):
-  ind = np.indices(dem.shape).transpose((1,2,0))
-  ind[flow ==   4] += np.array([ 1, 0])
-  ind[flow ==  64] += np.array([-1, 0])
+coords = [
+  [0, 0],
+  [1, 0],
+  [2, 0],
+  [0, 1],
+  [2, 1],
+  [0, 2],
+  [1, 2],
+  [2, 2]
+]
 
-  ind[flow ==   2] += np.array([ 1, 1])
-  ind[flow ==  32] += np.array([-1,-1])
+# Cost Matrix!
+cost = np.array([
+  [0, 1, 2, 3, 4, 3, 2, 1],
+  [1, 0, 1, 2, 3, 4, 3, 2],
+  [2, 1, 0, 1, 2, 3, 4, 3],
+  [3, 2, 1, 0, 1, 2, 3, 4],
+  [4, 3, 2, 1, 0, 1, 2, 3],
+  [3, 4, 3, 2, 1, 0, 1, 2],
+  [2, 3, 4, 3, 2, 1, 0, 1],
+  [1, 2, 3, 4, 3, 2, 1, 0]
+])
 
-  ind[flow ==   8] += np.array([ 1,-1])
-  ind[flow == 128] += np.array([-1, 1])
+def energy(state, area, i):
 
-  ind[flow ==   1] += np.array([ 0, 1])
-  ind[flow ==  16] += np.array([ 0,-1])
-  ind = np.clip(ind, (0,0), (dem.shape[0]-1, dem.shape[1]-1))
-  return ind
+  '''
+  Energy Function:
+  - Inward Pointing Neighbors Contribute
+  - Angle Determines Cost!
+  - Same Direction = 0
+  - Todo: Weight by Catchment Area
+  
+  Note: We can probably motivate the energy function
+    simply using the Cauchy-Momentum Equation.
+  '''
+
+  e = 0.0
+  t = dirmap[i]-1
+  for coord in coords:
+    s = state[coord[0], coord[1]]-1
+    a = area[coord[0], coord[1]]
+    if s < 0: continue  # Ignore Negative Values on the perimeter?
+    dir = dirset[s]
+    if coord[0] + dir[0] != 1: continue
+    if coord[1] + dir[1] != 1: continue
+    c = cost[s, t]
+    e += a*c*c  # Note: Square! (otherwise directionally biased)
+  return e
 
 def condition(model):
 
@@ -63,90 +113,71 @@ def condition(model):
   '''
 
   # Extract Initial Digital Elevation Model
-  grid, dem = model
+  grid, dem = model                               # DEM, Grid
+  flow = grid.flowdir(dem, dirmap=dirmap)         # Flow from DEM
+  area = grid.accumulation(flow, dirmap=dirmap)
+  ext = np.array([dem.shape[0], dem.shape[1]])
 
-  # Compute the Flowmap
-  dirmap = (64, 128, 1, 2, 4, 8, 16, 32)
-  flow = grid.flowdir(dem, dirmap=dirmap)
+  # First Step: Correct Pits?
 
-  # Initial Test: Basic Forward Propagate?
+  for n in range(1):
 
-  '''
-  Get the Flow Direction, Accumulation per Position
-  -> Advance the Positions to the next position  
-  '''
+    print("Invalid:", np.sum(flow < 0))
+    for x in range(ext[0]-2):
+      x += 1
+      for y in range(ext[1]-2):
+        y += 1
 
-#   flow_next = flow[ind[..., 0], ind[..., 1]]
-#   area_next = area[ind[..., 0], ind[..., 1]]
-#   flow_step = flow.copy()
+        if flow[x, y] > 0:
+          continue
+        
+        f_state = flow[x-1:x+2, y-1:y+2]
+        a_state = area[x-1:x+2, y-1:y+2]
 
-  # print("SHOWING PITS")
-  # plt.imshow(flow < 0)
-  # plt.show()
+        E = []
+        for i in range(8):
+          E = np.append(E, energy(f_state, a_state, i))
 
-  def opposite(flow, x, y, i):
-    if flow[i[0],i[1]] ==   1 and flow[x,y] ==  16: return True
-    if flow[i[0],i[1]] ==   2 and flow[x,y] ==  32: return True
-    if flow[i[0],i[1]] ==   4 and flow[x,y] ==  64: return True
-    if flow[i[0],i[1]] ==   8 and flow[x,y] == 128: return True
-    if flow[i[0],i[1]] ==  16 and flow[x,y] ==   1: return True
-    if flow[i[0],i[1]] ==  32 and flow[x,y] ==   2: return True
-    if flow[i[0],i[1]] ==  64 and flow[x,y] ==   4: return True
-    if flow[i[0],i[1]] == 128 and flow[x,y] ==   8: return True
-    return False
+        t = np.argmin(E)
+        flow[x, y] = dirmap[t]
 
-  # Do it energy based... makes thing simpler and more obvious
-  # what stable configurations are!
-  def energy(flow, x, y, i):
+  N = 32    # Number of Iterations
+  K = 2**16 # Samples per Iteration
 
-    # How do we do it?
+  for n in range(N):
 
-    return 0.0
-
-  for n in range(64):
-
-    print("Invalid:", np.sum(flow <= 0))
-    if(np.sum(flow <= 0) == 0):
-      break
-
-    ind = compute_index(dem, flow)
-
-    area = grid.accumulation(flow, dirmap=dirmap)
-
-    # Note: Remove Iteration Ordering Bias
+    print(n)
 
     flow_next = flow.copy()
+    area = grid.accumulation(flow, dirmap=dirmap)
 
-    print(2**16)
-    for k in range(2**16):
+    # Note: Add a way to compute total energy cost
 
-      # Sample Random Position
-      p = np.random.rand(2)
-      x = (dem.shape[0] * p[0]).astype(np.int64)
-      y = (dem.shape[1] * p[1]).astype(np.int64)
-      i = ind[x,y]
+    for k in tqdm(range(K)):
 
-      # Mutate
-      # Question: What is the op
+      # 1. Sample Random Position (Ignore Boundary)
+      p = 1 + (np.random.rand(2)*(ext-2)).astype(np.int64)
+      x = p[0]
+      y = p[1]
 
-      if flow[i[0],i[1]] == -1:
-        flow_next[i[0], i[1]] = flow[x,y]
+      # 2. Get Local State, Compute Energy Vector
 
-      if flow[i[0],i[1]] == -2:
-        flow_next[i[0], i[1]] = flow[x,y]
+      f_state = flow[x-1:x+2, y-1:y+2]
+      a_state = area[x-1:x+2, y-1:y+2]
 
-#      Correct Opposite Directions?
+      E = []
+      for i in range(8):
+        E = np.append(E, energy(f_state, a_state, i))
 
-      if opposite(flow, x, y, i):
-        if area[i[0],i[1]] > area[x,y]:
-          #Next is smaller: upstream
-          flow_next[x,y] = flow[i[0], i[1]]
-        else:
-          flow_next[i[0], i[1]] = flow[x,y]
+      # 3. Sampling Procedure
+      # -> Pick the Lowest Energy State
+      # -> Update the Local State
 
+      t = np.argmin(E)
+      flow_next[x, y] = dirmap[t]
+    
     flow = flow_next
 
-  print("Invalid: ", np.sum(flow < 0))
   return (grid, dem, dirmap, flow)
 
 '''
@@ -166,7 +197,6 @@ def flow(model):
 
   grid, dem = model
 
-  dirmap = (64, 128, 1, 2, 4, 8, 16, 32)
   fdir = grid.flowdir(dem, dirmap=dirmap)
 
   return (grid, dem, dirmap, fdir)
@@ -204,27 +234,28 @@ def plot_flow(model):
 
   fig = plt.figure(figsize=(8,6))
   fig.patch.set_alpha(0)
-  plt.imshow(fdir, extent=grid.extent, cmap='viridis', zorder=2)
+  plt.grid('on', zorder=0)
+
+  plt.imshow(fdir, zorder=2, cmap='viridis')
   boundaries = ([0] + sorted(list(dirmap)))
   plt.colorbar(boundaries= boundaries,
               values=sorted(dirmap))
   plt.xlabel('Longitude')
   plt.ylabel('Latitude')
   plt.title('Flow direction grid', size=14)
-  plt.grid(zorder=-1)
   plt.tight_layout()
   plt.show()
 
 def plot_acc(model):
 
   grid, acc = model
-  acc = np.transpose(acc)
+#  acc = np.transpose(acc)
 
   fig, ax = plt.subplots(figsize=(8,6))
   fig.patch.set_alpha(0)
   plt.grid('on', zorder=0)
-  im = ax.imshow(acc, zorder=2,
-                cmap='cubehelix',
+
+  im = ax.imshow(acc, zorder=2, cmap='cubehelix',
                 norm=colors.LogNorm(1, acc.max()),
                 interpolation='bilinear')
   plt.colorbar(im, ax=ax, label='Upstream Cells')
