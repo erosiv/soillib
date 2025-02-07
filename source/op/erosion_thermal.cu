3da8c9ca72630f67681d2247434ce86dec41f6d0
weigert <nicholasmcdonald40@gmail.com>
2025-02-07 19:43:05 +0100

    introduced laplacian thermal erosion implementation. works well but tends to oversmooth. needs to also have a limiting value or some way to scale

--- source/op/erosion_thermal.cu
+++ source/op/erosion_thermal.cu
@@ -1,41 +1,66 @@
+#ifndef SOILLIB_OP_EROSION_THERMAL_CU
+#define SOILLIB_OP_EROSION_THERMAL_CU
+#define HAS_CUDA
+
+#include <soillib/core/types.hpp>
+#include <soillib/core/buffer.hpp>
+#include <soillib/core/index.hpp>
+
+#include <soillib/op/erosion.hpp>
+
+//! Thermal Erosion Algorithms
+//!
+//! These device functions implement various methods to compute
+//! the time-differential of a height-map from thermal erosion.
+//! 
+//! \todo Research alternative thermal erosion implementations
+
 namespace soil {
 
+//! Slope Limiting Mass Wasting
+__device__ void slope_limit(const buffer_t<float>& height, buffer_t<float>& height_diff, const flat_t<2>& index, const ivec2 ipos, const param_t param) {
 
-//
-// Cascading Kernel
-//
-// Effectively we have to compute the height-difference between every cell
-// and its non-out-of-bounds neighbors, then we have to transfer the sediment.
-// How do we do this without race conditions?
-// I suppose that we need an additional buffer to determine the updated sediment amounts...
-// so that we can ping-pong back and forth...
-
-// for now, we will implement this as a device function locally and perhaps switch to
-// a singular kernel later.
-
-// __global__ void cascade(model_t model){
-// }
-
-__device__ void cascade(model_t& model, const glm::ivec2 ipos, buffer_t<float>& transfer, const param_t param) {
-
-  if(model.index.oob(ipos))
+  if(index.oob(ipos))
     return;
 
   // Get Non-Out-of-Bounds Neighbors
 
-  const glm::ivec2 n[] = {
-    glm::ivec2(-1, -1),
-    glm::ivec2(-1, 0),
-    glm::ivec2(-1, 1),
-    glm::ivec2(0, -1),
-    glm::ivec2(0, 1),
-    glm::ivec2(1, -1),
-    glm::ivec2(1, 0),
-    glm::ivec2(1, 1)
+  const ivec2 n[] = {
+    ivec2(-1, -1),
+    ivec2(-1, 0),
+    ivec2(-1, 1),
+    ivec2(0, -1),
+    ivec2(0, 1),
+    ivec2(1, -1),
+    ivec2(1, 0),
+    ivec2(1, 1)
+  };
+
+  /*
+  const float w[] = {
+    0.4f,
+    0.4f,
+    0.4f,
+    2.0f,
+    2.0f,
+    4.0f,
+    4.0f,
+    4.0f
+  };
+  */
+
+  const float w[] = {
+    1.0,
+    1.0,
+    1.0,
+    1.0,
+    1.0,
+    1.0,
+    1.0,
+    1.0
   };
 
   struct Point {
-    glm::ivec2 pos;
     float h;
     float d;
   } sn[8];
@@ -44,36 +69,30 @@ __device__ void cascade(model_t& model, const glm::ivec2 ipos, buffer_t<float>& 
 
   for(auto &nn : n){
 
-    glm::ivec2 npos = ipos + nn;
+    ivec2 npos = ipos + nn;
 
-    if(model.index.oob(npos))
+    if(index.oob(npos))
       continue;
 
-    const size_t index = model.index.flatten(npos);
-    const float height = model.height[index];
-    sn[num] = {npos, height, length(glm::vec2(nn))};
+    const size_t i = index.flatten(npos);
+    sn[num] = {height[i], length(vec2(nn))};
     ++num;
   }
 
-  const size_t index = model.index.flatten(ipos);
-  const float height = model.height[index];
-  float h_ave = height;
-  // for (int i = 0; i < num; ++i)
-  //   h_ave += sn[i].h;
-  // h_ave /= (float)(num + 1);
-
+  const size_t i = index.flatten(ipos);
+  const float h = height[i];
   float transfer_tot = 0.0f;
   
   for(int i = 0; i < num; ++i){
 
     // Full Height-Different Between Positions!
-    float diff = h_ave - sn[i].h;
+    float diff = h - sn[i].h;
     if (diff == 0) // No Height Difference
       continue;
 
     // The Amount of Excess Difference!
     float excess = 0.0f;
-    excess = abs(diff) - sn[i].d * param.maxdiff;
+    excess = abs(diff) - sn[i].d * w[i]* param.maxdiff;
     if (excess <= 0) // No Excess
       continue;
 
@@ -84,20 +103,46 @@ __device__ void cascade(model_t& model, const glm::ivec2 ipos, buffer_t<float>& 
     transfer_tot += transfer;
   }
 
-  transfer[index] = transfer_tot / (float) num;
+  height_diff[i] = transfer_tot / (float) num;
+
 }
 
-__global__ void compute_cascade(model_t model, buffer_t<float> transfer, const param_t param){
+//! Laplacian Based Mass Wasting Function
+//!
+__device__ void thermal_laplacian(const buffer_t<float>& height, buffer_t<float>& height_diff, const flat_t<2>& index, const ivec2 ipos, const param_t param) {
 
-  // note: this should be two kernels.
-  // one to compute the amount of sediment
-  // that has to be transferred -
-  // one to then actual transfer it.
+  if(index.oob(ipos))
+    return;
+
+  const size_t i = index.flatten(ipos + ivec2( 0, 0));
+  float ct = height[i];
+  float nx = ct;
+  float ny = ct;
+  float px = ct;
+  float py = ct;
+  if(!index.oob(ipos + ivec2( 1, 0))) px = height[index.flatten(ipos + ivec2( 1, 0))];
+  if(!index.oob(ipos + ivec2(-1, 0))) nx = height[index.flatten(ipos + ivec2(-1, 0))];
+  if(!index.oob(ipos + ivec2( 0, 1))) py = height[index.flatten(ipos + ivec2( 0, 1))];
+  if(!index.oob(ipos + ivec2( 0,-1))) ny = height[index.flatten(ipos + ivec2( 0,-1))];
+
+  float lx = px + nx - 2.0f * ct;
+  float ly = py + ny - 2.0f * ct;
+  height_diff[i] = param.settling * (lx + ly);
+
+}
+
+//
+// Thermal Erosion Kernels
+//
+
+__global__ void compute_cascade(model_t model, buffer_t<float> transfer, const param_t param) {
 
   const unsigned int ind = blockIdx.x * blockDim.x + threadIdx.x;
   if(ind >= model.elem) return;
   const ivec2 ipos = model.index.unflatten(ind);
-  cascade(model, ipos, transfer, param);
+
+  slope_limit(model.height, transfer, model.index, ipos, param);
+  //thermal_laplacian(model.height, transfer, model.index, ipos, param);
 
 }
 
@@ -113,4 +158,6 @@ __global__ void apply_cascade(model_t model, buffer_t<float> transfer_b, const p
   //model.height[ind] += transfer;
 }
 
-}
+} // end of namespace soil
+
+#endif
