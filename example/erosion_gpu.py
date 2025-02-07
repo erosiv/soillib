c53caec82e0903ea9849966e0452efa9eb86f4a3
weigert <nicholasmcdonald40@gmail.com>
2025-02-07 10:37:06 +0100

    Fixed the erosion example and removed redundant examples

--- example/erosion_gpu.py
+++ example/erosion_gpu.py
@@ -6,28 +6,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
-def relief_shade(h, n):
-
-  # Regularize Height
-  h_min = np.nanmin(h)
-  h_max = np.nanmax(h)
-  h = (h - h_min)/(h_max - h_min)
-
-  # Light Direction, Diffuse Lighting
-  light = np.array([-1, 1, 2])
-  light = light / np.linalg.norm(light)
-
-  diffuse = np.sum(light * n, axis=-1)
-  diffuse = 0.05 + 0.9*diffuse
-
-  # Flat-Toning
-  flattone = np.full(h.shape, 0.9)
-  weight = 1.0 - n[:,:,2]
-  weight = weight * (1.0 - h * h)
-
-  # Full Diffuse Shading Value
-  diffuse = (1.0 - weight) * diffuse + weight * flattone
-  return diffuse
+from __common__ import relief_shade
 
 def main():
 
@@ -47,18 +26,34 @@ def main():
   soil.set(momentum, [0.0, 0.0])
   momentum.gpu()
 
+  suspended = soil.buffer(soil.float32, index.elem())
+  soil.set(suspended, 0.0)
+  suspended.gpu()
+
   model = soil.model_t(index)
   model.height = buffer
   model.discharge = discharge
   model.momentum = momentum
+  model.suspended = suspended
 
   param = soil.param_t()
-  param.lrate = 0.1
-  param.momentumTransfer = 1.0
-  param.gravity = 1.0
+  param.gravity = 1
+  param.momentumTransfer = 0.25
+  param.maxdiff = 0.75
+  param.settling = 0.75
+  param.depositionRate = 0.25
+  param.entrainment = 0.125
+  param.lrate = 0.125
+  param.exitSlope = 0.0
+  param.maxage = 1024
+  param.hscale = 0.01
+  param.evapRate = 0.001
 
-  with soil.timer() as timer:
-    soil.gpu_erode(model, param, 2048)
+  timer = soil.timer()
+  for i in range(16):
+    with timer:
+      soil.gpu_erode(model, param, 4, 8192)
+    print(f"Execution Time: {timer.count}ms")
 
   buffer.cpu()
   discharge.cpu()
@@ -66,26 +61,25 @@ def main():
   tiff_out = soil.tiff(buffer, index)
   tiff_out.write("/home/nickmcdonald/Datasets/erosion_gpu.tiff")
 
-#  normal = soil.normal(buffer, index)
-  height = buffer.numpy(index)
+  normal = soil.normal(buffer, index)
+  height = model.height
+  height.cpu()
+  height = height.numpy(index)
   print(np.max(height))
   print(np.min(height))
 
-#
 #  plt.imshow(height)
 #  plt.show()
 #
-#  normal = normal.numpy(index)
+#  return
 #
-#  relief = relief_shade(height, normal)
-#
-
-
-#  plt.imshow(relief, cmap='gray')
+  normal = normal.numpy(index)
+  relief = relief_shade(height, normal)
+  plt.imshow(relief, cmap='gray')
 
 
 
-  plt.imshow(np.log(1.0 + discharge.numpy(index)))
+#  plt.imshow(np.log(1.0 + discharge.numpy(index)))
   #plt.imshow(discharge.numpy(index)[4:-2, 4:-2])
   plt.show()
 
