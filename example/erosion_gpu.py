5ae1d641cea84ec812286b83e8d8ed28bdf34f0e
weigert <nicholasmcdonald40@gmail.com>
2025-02-22 13:19:00 +0100

    fixed erosion example and commented on most parameters

--- example/erosion_gpu.py
+++ example/erosion_gpu.py
@@ -10,39 +10,51 @@ from __common__ import relief_shade
 
 def main():
 
-  index = soil.index([512, 512])
+  index = soil.index([512, 512])  # Simulation Resolution
+  wscale = [40.0, 40.0, 4.0]      # World Scale [km] (x, y, z)
+  nscale = [20.0, 20.0]           # Noise Feature Scale [km] (x, y)
+  scale = [ wscale[0]/index[0],   # Pixel Scale [km]
+            wscale[2], 
+            wscale[1]/index[1]] 
 
-  seed = 0
-  height = soil.noise(index, seed)
-  soil.multiply(height, 80.0)
+  noise_param = soil.noise_t()
+  noise_param.ext = np.array([index[0], index[1]]) * np.array(nscale) / np.array([wscale[0], wscale[1]])
+  noise_param.seed = 0
 
+  height = soil.noise(index, noise_param)
+  soil.multiply(height, 1.0)
+
+  sediment = soil.buffer(soil.float32, index.elem(), soil.gpu)
   discharge = soil.buffer(soil.float32, index.elem(), soil.gpu)
-  suspended = soil.buffer(soil.float32, index.elem(), soil.gpu)
   momentum = soil.buffer(soil.vec2, index.elem(), soil.gpu)
   
+  sediment[:] = 0.0
   discharge[:] = 0.0
-  suspended[:] = 0.0
   momentum[:] = [0.0, 0.0]
-
-  model = soil.model_t(index)
+  
+  model = soil.model_t(index, scale)
   model.height = height.gpu()
+  model.sediment = sediment
   model.discharge = discharge
   model.momentum = momentum
-  model.suspended = suspended
 
   param = soil.param_t()
-  param.samples = 8192
-  param.gravity = 1
-  param.viscosity = 0.25
-  param.maxdiff = 0.75
-  param.settling = 0.75
-  param.depositionRate = 0.1
-  param.entrainment = 0.125
-  param.lrate = 0.125
-  param.exitSlope = 0.0
-  param.maxage = 1024
-  param.hscale = 0.01
-  param.evapRate = 0.001
+  param.samples = 8192  # Number of Path Samples
+  param.maxage = 128    # Maximum Path Length
+  param.lrate = 0.2     # Filter Learning Rate
+
+  param.rainfall = 2.0      # Rainfall Rate [m/y]
+  param.evapRate = 0.0001   # Evaporation Rate [1/s]
+
+  param.gravity = 9.81      # Specific Gravity [m/s^2]
+  param.viscosity = 0.03    # Kinematic Viscosity [m^2/s]
+
+  param.maxdiff = 0.57      # Critical Slope [m/m]
+  param.settling = 0.005    # Thermal Erosion Rate
+
+  param.depositionRate = 0.05 # Fluvial Deposition Rate
+  param.entrainment = 0.00025 # Fluvial Suspension Rate
+  param.exitSlope = 0.01      # Boundary Slope [m/m]
 
   timer = soil.timer()
   for i in range(1024):
