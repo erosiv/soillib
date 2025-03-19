7c7eaf5af21171d55fb001fcfe4c3c12868ee508
weigert <nicholasmcdonald40@gmail.com>
2025-03-19 16:56:59 +0100

    added basic pointcloud sampling with nan ignoring

--- source/soillib/op/pointcloud.cu
+++ source/soillib/op/pointcloud.cu
@@ -6,55 +6,57 @@
 #include <soillib/core/buffer.hpp>
 #include <soillib/core/index.hpp>
 
+#include <curand_kernel.h>
 #include <soillib/op/common.hpp>
+#include <soillib/op/gather.hpp>
 
 namespace soil {
+
+namespace {
+  __global__ void seed(buffer_t<curandState> buffer, const size_t seed, const size_t offset) {
+    const unsigned int n = blockIdx.x * blockDim.x + threadIdx.x;
+    if(n >= buffer.elem()) return;
+    curand_init(seed, n, offset, &buffer[n]);
+  }
+}
+
+//template<typename T, typename Index, typename Flat>
+__global__ void _sample_pointcloud(soil::buffer_t<float> input, soil::buffer_t<vec3> output, soil::buffer_t<curandState> rand, const soil::flat_t<2> index, const size_t N){
+
+  const unsigned int n = blockIdx.x * blockDim.x + threadIdx.x;
+  if(n >= N) return;
+
+  curandState* randState = &rand[n];
+  vec2 pos = vec2{
+    curand_uniform(randState)*float(index[0]),
+    curand_uniform(randState)*float(index[1])
+  };
+  
+  lerp_t<float> lerp = gather(input, index, pos);
+  while(isnan(lerp.val())){
+    pos = vec2{
+      curand_uniform(randState)*float(index[0]),
+      curand_uniform(randState)*float(index[1])
+    };
+    lerp = gather(input, index, pos);
+  }
+  output[n] = vec3(pos.x, pos.y, lerp.val());
+
+}
 
 soil::buffer_t<vec3> sample_pointcloud_impl(const soil::buffer_t<float> &buffer, const soil::index &index, const size_t N){
 
   soil::buffer_t<vec3> output(N, soil::GPU);
-  soil::set(output, vec3(0.0f));
+  soil::buffer_t<curandState> rand(N, soil::host_t::GPU);
+
+  const auto index_t = index.as<soil::flat_t<2>>();
+  seed<<<block(N, 1024), 1024>>>(rand, 0, 0);
+  cudaDeviceSynchronize();
+  _sample_pointcloud<<<block(N, 1024), 1024>>>(buffer, output, rand, index_t, N);
+
   return output;
 
 }
-
-/*
-template<typename T, typename Index, typename Flat>
-__global__ void _resample(soil::buffer_t<T> input, soil::buffer_t<T> output, const Index index, const Flat flat){
-  const unsigned int n = blockIdx.x * blockDim.x + threadIdx.x;
-  if(n >= flat.elem()) return;
-  
-  auto pos = flat.unflatten(n);
-  if(!index.oob(pos)){
-    output[n] = input[index.flatten(pos)];
-  }
-}
-
-template<typename T>
-soil::buffer_t<T> resample_impl(const soil::buffer_t<T>& input, const soil::index& index){
-  
-return select(index.type(), [&]<typename I>(){
-  
-auto index_t = index.as<I>();
-soil::flat_t<I::n_dims> flat(index_t.ext());
-
-soil::buffer_t<T> output(flat.elem(), soil::GPU);
-using V = soil::typedesc<T>::value_t;
-T value = T{std::numeric_limits<V>::quiet_NaN()};
-set_impl<T>(output, value, 0, flat.elem(), 1);
-
-int thread = 1024;
-int elem = flat.elem();
-int block = (elem + thread - 1)/thread;
-_resample<<<block, thread>>>(input, output, index_t, flat);
-
-return output;
-
-});
-
-}
-*/
-
 
 }
 
