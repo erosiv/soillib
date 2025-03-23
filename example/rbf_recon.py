296275037c56c37f6ae47b84cd859c95f5a57be2
weigert <nicholasmcdonald40@gmail.com>
2025-03-23 20:10:04 +0100

    functioning multiscale separated layers but needs higher resolution refinement

--- example/rbf_recon.py
+++ example/rbf_recon.py
@@ -31,8 +31,7 @@ def fullimage(index):
     torch.linspace(0, index[1]-1, index[1]),
     indexing='ij')
   pos = torch.stack((posx, posy), dim=-1)
-  print(pos.shape)
-  return pos
+  return pos.to(device='cuda')
 
 def sparseimage(index, K = 1024):
   shape = torch.Tensor([index[0], index[1]])
@@ -40,26 +39,25 @@ def sparseimage(index, K = 1024):
 
 def sampleimage(image, index, pos):
   ipos = pos.to(dtype = torch.int)
-  test = image[ipos[:, 0], ipos[:, 1]]
+  test = image[ipos[..., 0], ipos[..., 1]]
   return test
 
 def resize(image, size):
   view = image.unsqueeze(-1).permute(2, 0, 1).unsqueeze(0)
   view = torch.nn.functional.interpolate(view, size=size) # mode='bilinear'
-  return view[0].permute(1, 2, 0)
+  return view[0].permute(1, 2, 0).squeeze(-1)
 
 def fmap(dist, shape):
   return 1.0 / (1.0 + (shape*shape*dist*dist))
 
 class RBFLayer:
 
-  def __init__(self, index, K):
+  def __init__(self, centers):
 
-    self.K = K
-    self.shape = torch.Tensor([index[0], index[1]])
-    self.centers = (torch.rand(K, 2)*self.shape).to(device='cuda')
-    self.weights = (torch.full((K,), 0.0)).to(device='cuda')
-    self.shapes = (torch.full((K,), 0.02)).to(device='cuda')
+    self.centers = centers
+    self.K = self.centers.shape[0]
+    self.weights = (torch.full((self.K,), 0.0)).to(device='cuda')
+    self.shapes = (torch.full((self.K,), 0.02)).to(device='cuda')
 
   def sample(self, pos):
     dist = torch.cdist(pos, self.centers, p=2)
@@ -82,26 +80,9 @@ class RBFLayer:
         image += vals.squeeze(-1)*self.weights[k]
       return image
 
-class RBFInterpolator:
+  def fit(self, pos, val, steps = 2048):
 
-  def __init__(self, index, K):
-    self.layers = [ RBFLayer(index, k) for k in K ]
-
-  def sample(self, pos):
-    approx = torch.full((pos.shape[0],), 0.0, device='cuda')
-    for layer in self.layers:
-      approx += layer.sample(pos)
-    return approx
-
-  def full(self, index):
-    image = torch.full((index[0], index[1]), 0.0, device='cuda')
-    for layer in self.layers:
-      image += layer.full(index)
-    return image
-
-  def fit(self, pos, val, layer, steps = 2048,  lr = 0.01):
-
-    params = self.layers[layer].params()
+    params = self.params()
     optimizer = torch.optim.Adam([
       {'params': [params[0]], 'lr': 1e-2},
       {'params': [params[1]], 'lr': 1e-2},
@@ -115,10 +96,67 @@ class RBFInterpolator:
         loss = torch.mean((val-approx)*(val-approx))
         loss.backward()
         optimizer.step()
+        t.set_description(f"Loss ({loss.item():.3f})")
+
+    for param in params:
+      param.requires_grad = False
+
+'''
+class RBFInterpolator:
+
+  def __init__(self, index, K):
+    self.layers = [ RBFLayer(index, k) for k in K ]
+
+  def sample(self, pos):
+    approx = torch.full(pos.shape[:-1], 0.0, device='cuda')
+    for layer in self.layers:
+      approx += layer.sample(pos)
+    return approx
+
+  def full(self, index):
+    image = torch.full((index[0], index[1]), 0.0, device='cuda')
+    for layer in self.layers:
+      image += layer.full(index)
+    return image
+
+  def fit(self, pos, val, layer, steps = 2048):
+
+    params = self.layers[layer].params()
+    optimizer = torch.optim.Adam([
+      {'params': [params[0]], 'lr': 1e-2},
+      {'params': [params[1]], 'lr': 1e-2},
+      {'params': [params[2]], 'lr': 1e-3},
+    ])
+
+    with tqdm(range(steps)) as t:
+      for step in t:
+        optimizer.zero_grad()
+        approx = self.sample(pos)
+        loss = torch.mean((val-approx)*(val-approx))
+        loss.backward()
+        optimizer.step()
         t.set_description(f"Loss ({loss.item()})")
 
     for param in params:
       param.requires_grad = False
+'''
+
+class RBFInterpolator:
+
+  def __init__(self, layers):
+    self.layers = layers
+
+  def sample(self, pos):
+    approx = torch.full(pos.shape[:-1], 0.0, device='cuda')
+    for layer in self.layers:
+      approx += layer.sample(pos)
+    return approx
+
+  def full(self, index):
+    image = torch.full((index[0], index[1]), 0.0, device='cuda')
+    for layer in self.layers:
+      image += layer.full(index)
+    return image
 
 def main(input):
 
@@ -129,32 +167,112 @@ def main(input):
     index = image.index
     buffer = image.buffer.gpu().torch(index)
 
+    steps = 4095
+    layers = []
+
+    # Downsample Image
+    shape = (16, 16)
+    buffer16 = resize(buffer, shape)
+    pos = fullimage(shape)
+    val = sampleimage(buffer16, shape, pos)
+
+    # Lowest-Level Interpolatin Tensor
+    centers = (1 + fullimage((1, 1)))/2
+    centers = centers * 512
+    print(centers.shape)
+
+    rbf = RBFLayer(centers.to(device='cuda'))
+    layers.append(rbf)
+
+    rbf.fit(32*pos, val, steps)
+
+    recon = rbf.full(index)
+    buffer = buffer - recon
+
+    # Downsample Image
+    shape = (16, 16)
+    buffer16 = resize(buffer, shape)
+    pos = fullimage(shape)
+    val = sampleimage(buffer16, shape, pos)
+
+    centers = (1 + fullimage((2, 2)))/3
+    centers = centers * 512
+    centers = centers.view((4, 2))
     
+    rbf = RBFLayer(centers.to(device='cuda'))
+    layers.append(rbf)
 
+    rbf.fit(32*pos, val, steps)
 
-    K = [1]#, 64, 128]
-    rbf = RBFInterpolator(index, K)
+    recon = rbf.full(index)
+    buffer = buffer - recon
 
-    #torch.nn.functional.interpolate(input, size=None, scale_factor=None, mode='nearest', align_corners=None, recompute_scale_factor=None, antialias=False)[source]
-    pos = sparseimage(index, 1024)
-    val = sampleimage(buffer, index, pos)
+    # Downsample Image
+    shape = (16, 16)
+    buffer16 = resize(buffer, shape)
+    pos = fullimage(shape)
+    val = sampleimage(buffer16, shape, pos)
 
-    for i, k in enumerate(K):
-      print(f"Converging Iteration {i}")
-      rbf.fit(pos, val, i, 128)
+    centers = (1 + fullimage((4, 4)))/5
+    centers = centers * 512
+    centers = centers.view((16, 2))
+    print(centers.shape)
 
+    rbf = RBFLayer(centers.to(device='cuda'))
+    layers.append(rbf)
 
+    rbf.fit(32*pos, val, steps)
 
+    recon = rbf.full(index)
+    buffer = buffer - recon
 
+    # Downsample Image
+    shape = (16, 16)
+    buffer16 = resize(buffer, shape)
+    pos = fullimage(shape)
+    val = sampleimage(buffer16, shape, pos)
 
+    centers = (1 + fullimage((8, 8)))/9
+    centers = centers * 512
+    centers = centers.view((64, 2))
+    print(centers.shape)
 
+    rbf = RBFLayer(centers.to(device='cuda'))
+    layers.append(rbf)
 
+    rbf.fit(32*pos, val, steps)
 
+    recon = rbf.full(index)
+    buffer = buffer - recon
+
+    # Downsample Image
+    shape = (16, 16)
+    buffer16 = resize(buffer, shape)
+    pos = fullimage(shape)
+    val = sampleimage(buffer16, shape, pos)
+
+    centers = (1 + fullimage((16, 16)))/17
+    centers = centers * 512
+    centers = centers.view((16*16, 2))
+    print(centers.shape)
+
+    rbf = RBFLayer(centers.to(device='cuda'))
+    layers.append(rbf)
+
+    rbf.fit(32*pos, val, steps)
+
+    recon = rbf.full(index)
+    buffer = buffer - recon
 
     '''
     Visualization Code
     '''
 
+
+
+
+
+    rbf = RBFInterpolator(layers)
     newimage = rbf.full(index)
     height_new = newimage.cpu().numpy()
 #    height_new = soil.buffer.from_numpy(newimage.cpu().numpy())
@@ -162,7 +280,8 @@ def main(input):
     #height_new = height_new.numpy(index)
 
 #    resize_transform = transforms.Resize((16, 16))
-    buffer = resize(buffer, (16, 16))
+    buffer = image.buffer.gpu().torch(index)
+#    buffer = resize(buffer, (16, 16))
     height = buffer.cpu().numpy()
 
     #normal = soil.normal(image.buffer, index, image.meta.scale).numpy(index)
@@ -171,9 +290,13 @@ def main(input):
 #    relief_new = relief_shade(height_new, normal_new)
 #    relief = relief_shade(height, normal)
 
+    vmin = np.min(height)
+    vmax = np.max(height)
+    print("MEAN", np.mean(height))
+
     fig, axs = plt.subplots(1,2)
-    axs[0].imshow(height_new, cmap='gray')
-    axs[1].imshow(height, cmap='gray')
+    axs[0].imshow(height_new, cmap='gray', vmin=vmin, vmax=vmax)
+    axs[1].imshow(height, cmap='gray', vmin=vmin, vmax=vmax)
 
     plt.show()
 
