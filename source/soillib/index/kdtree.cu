4cb73c81d18d03e1c23faaf47ba5eaf0ea7666e1
weigert <nicholasmcdonald40@gmail.com>
2025-04-19 17:53:39 +0200

    reverted to 3D because of the way the pointcloud is sampled. really, I need to separate the indices first for this to work

--- source/soillib/index/kdtree.cu
+++ source/soillib/index/kdtree.cu
@@ -13,16 +13,16 @@ namespace soil {
 // KDTree Implementation
 //
 
-struct PointPlusPayload_traits: public cukd::default_data_traits<float2> {
+struct payload_traits: public cukd::default_data_traits<float3> {
  
-  using point_t = float2;
+  using point_t = float3;
 
   static inline __device__ __host__
   point_t get_point(const payload_t &data)
   { return data.p; }
 
   static inline __device__ __host__
-  float  get_coord(const payload_t &data, int dim)
+  float get_coord(const payload_t &data, int dim)
   { return cukd::get_coord(get_point(data),dim); }
 
   enum { has_explicit_dim = false };
@@ -36,28 +36,28 @@ inline int block(const int elem, const int thread) {
   return (elem + thread - 1) / thread;
 }
 
-__global__ void _kdtreecopy(buffer_t<payload_t> out, const soil::buffer_t<vec2> source) {
+__global__ void _kdtreecopy(buffer_t<payload_t> out, const soil::buffer_t<vec3> source) {
   const unsigned int n = blockIdx.x * blockDim.x + threadIdx.x;
   if(n >= out.elem()) return;
-  const vec2 p = source[n];
-  out[n].p = make_float2(p.x, p.y);
+  const vec3 p = source[n];
+  out[n].p = make_float3(p.x, p.y, p.z);
   out[n].i = n;
 }
 
-__global__ void _knnquery(const buffer_t<vec2> query_b, const buffer_t<payload_t> data, soil::buffer_t<int> output, const size_t K){
+__global__ void _knnquery(const buffer_t<vec3> query_b, const buffer_t<payload_t> data, soil::buffer_t<int> output, const size_t K){
 
   const unsigned int n = blockIdx.x * blockDim.x + threadIdx.x;
   if(n >= query_b.elem()) return;
 
-  const vec2 q = query_b[n];
+  const vec3 q = query_b[n];
 
   // Candidate List, Query Result
   cukd::HeapCandidateList<16> result(100.0);
   cukd::stackBased::knn<
     cukd::HeapCandidateList<16>,
     payload_t,
-    PointPlusPayload_traits
-  >(result, make_float2(q.x, q.y), data.data(), data.elem());
+    payload_traits
+  >(result, make_float3(q.x, q.y, q.z), data.data(), data.elem());
 
   for(int k = 0; k < K; ++k) {
     int ID = result.get_pointID(k);
@@ -72,22 +72,16 @@ __global__ void _knnquery(const buffer_t<vec2> query_b, const buffer_t<payload_t
 
 }
 
-void kdtree::setup(const buffer_t<vec2>& source) {
+void kdtree::setup(const buffer_t<vec3>& source) {
   _kdtreecopy<<<block(this->elem(), 1024), 1024>>>(this->data, source);
   cukd::buildTree<
     payload_t,
-    PointPlusPayload_traits
+    payload_traits
   >(this->data.data(), this->elem());
 }
 
-buffer_t<int> kdtree::knnq(const buffer_t<vec2>& query, const size_t k) const {
-
-  const size_t n_query = query.elem()/2;
-
-  auto output = soil::buffer_t<int>{k * n_query, soil::host_t::GPU};
-  _knnquery<<<block(n_query, 512), 512>>>(query, this->data, output, k);
-  return output;
-
+void kdtree::knni(const buffer_t<vec3>& query, const size_t k, buffer_t<int>& indices) const {
+  _knnquery<<<block(indices.elem(), 512), 512>>>(query, this->data, indices, k);
 }
 
 } // end of namespace soil::
--- source/soillib/index/kdtree.hpp
+++ source/soillib/index/kdtree.hpp
@@ -6,11 +6,15 @@
 
 namespace soil {
 
+template<typename T>
+concept vectortype = (
+//  std::same_as<T, soil::vec2> ||
+  std::same_as<T, soil::vec3>
+);
+
 struct payload_t {
-
-  float2 p;
+  float3 p;
   size_t i;
-
 };
 
 //! kdtree is a wrapper for a kdtree type
@@ -19,9 +23,11 @@ struct payload_t {
 //! which can be used to then index separate buffers.
 struct kdtree {
 
-  kdtree(const soil::buffer& buffer){
-    this->data = buffer_t<payload_t>(buffer.elem(), soil::host_t::GPU);
-    this->setup(buffer.as<vec2>());
+  kdtree(const soil::buffer& buffer) {
+    soil::select(buffer.type(), [&]<vectortype T>(){
+      this->data = buffer_t<payload_t>(buffer.elem(), soil::host_t::GPU);
+      this->setup(buffer.as<vec3>());
+    });
   }
 
   //
@@ -32,14 +38,18 @@ struct kdtree {
   //!
   //! Note that the data is unchanged, and any query operations
   //! return a buffer of indices which align to this buffer.
-  void setup(const buffer_t<vec2>& buffer);
+  void setup(const buffer_t<vec3>& buffer);
 
   //! Execute a k-nearest neighbors search, returning indices
   buffer_t<int> knn(const buffer& query, const size_t k) const {
-    return this->knnq(query.as<vec2>(), k);
+    const size_t n_query = query.elem()/3;
+    auto indices = soil::buffer_t<int>{k * n_query, soil::host_t::GPU};
+    this->knni(query.as<vec3>(), k, indices);
+    return indices;
   }
 
-  buffer_t<int> knnq(const buffer_t<vec2>& query, const size_t k) const;
+  //! Execute a k-nearest neighbors with existing indices
+  void knni(const buffer_t<vec3>& query, const size_t k, buffer_t<int>& indices) const;
 
   //
   // Data Inspection
