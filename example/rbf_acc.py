95e0fd3e89d09934059d276ab1967336755209e4
weigert <nicholasmcdonald40@gmail.com>
2025-04-28 14:32:23 +0200

    added a from torch method for buffer construction based on view and copy

--- example/rbf_acc.py
+++ example/rbf_acc.py
@@ -9,27 +9,11 @@ import torch
 
 '''
 radial basis functions (with kdtree)
+sparse accumulation and erosion kernels
 
-This is an improved version of radial basis
-function fitting with added polynomial support.
-
-The computation can be accelerated with a kdtree.
-Currently, the fitting procedure occurs in pytorch
-using a linear algebra least-squares solver, which
-could potentially be moved into C++ for uniformity.
-
-Todo:
-- Test Application in CUDA Code
-- Multi-Scale RBF?
-
-In principle, we could say that a small number
-of radial basis functions is always applied to
-solve the broad shape. We can even use multiple
-kdtrees to accelerate the multi-layer lookup,
-or simply ignore that for certain layers.
-    
-This could dramatically improve the fit overall,
-especially with position optimization.
+1. Compute the RBF representation
+2. Launch the sparse accumulation kernel
+3. Visualize the result...
 '''
 
 def main(input):
@@ -45,16 +29,12 @@ def main(input):
 
     print("Sampling Digital Elevation Model...")
 
-    # Note: Replace with e.g. Halton Sampler
-
     K = 4096
     N = 8 * K
 
-#    center = soil.sampleN(index, K)
     center = soil.sample_halton(index, K)
     sample = soil.sampleN(index, N)
     values = soil.sample_lerp(buffer, index, sample)
-    #normal = soil.sample_grad(buffer, index, sample)
 
     print("Initializing Radial Basis Function Interpolator...")
 
@@ -72,9 +52,6 @@ def main(input):
     tvector = vector.torch(soil.index([N+rbf.P]))
 
     w = torch.linalg.lstsq(tmatrix, tvector).solution
-
-    print(torch.mean(torch.abs(w)))
-
     rbf.set_w(soil.buffer.from_torch(w))
 
     print("Computing Estimation Error...")
