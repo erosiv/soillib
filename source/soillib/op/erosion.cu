7ca55630896f4a680441ac8dfb2784d49c9e91da
weigert <nicholasmcdonald40@gmail.com>
2025-04-30 19:35:28 +0200

    more code simplification

--- source/soillib/op/erosion.cu
+++ source/soillib/op/erosion.cu
@@ -41,6 +41,13 @@ struct particle_t {
 // Model Geometry and Lookup Procedures
 //
 
+
+__device__ int __nearest(const model_t& model, const vec2 pos){
+
+  return model.index.flatten(pos);
+
+}
+
 __device__ vec3 __normal(const model_t& model, const vec2 pos, const vec3 scale){
 
   lerp5_t<float> lerp;
@@ -50,24 +57,40 @@ __device__ vec3 __normal(const model_t& model, const vec2 pos, const vec3 scale)
 
 }
 
-__device__ int __nearest(const model_t& model, const vec2 pos){
-  return model.index.flatten(pos);
+__device__ float __slope(const model_t& model, const particle_t& part, const param_t& param){
+
+  const vec3 scale = model.scale * 1E3f;  // Cell Scale [m] (conv. from km)
+  const vec2 cl = vec2(scale.x, scale.y); // Cell Length [m, m]
+
+  if(model.index.oob(part.npos)){
+    return -param.exitSlope;
+  }
+
+  const int nind = model.index.flatten(part.npos);
+  float h0 = (model.height[part.ind] + model.sediment[part.ind])*scale.z;
+  float h1 = (model.height[nind] + model.sediment[nind])*scale.z;
+  return (h1 - h0)/glm::length(cl);
+
 }
 
+//
+// Derived Quantities
+//
+
 __device__ vec2 __avespeed(const model_t& model, const particle_t& part){
-  const vec2 momentum = model.momentum[part.ind];
+  
   const float discharge = model.discharge[part.ind];
   if(discharge == 0.0f)
     return vec2(0.0f);
+  
+  const vec2 momentum = model.momentum[part.ind];
   return momentum / discharge;
+
 }
 
 //
-// Position Sampling Procedure:
-//  Choose a sampling method, determine the sampling probability,
-//  determine the contribution weight from the sample count.
-//  Then get the sample and determine the nearest point.
-//
+// Core Procedures
+// 
 
 //! Position Sampling Procedure
 //!
@@ -95,22 +118,22 @@ __device__ void __init(particle_t& part, const model_t& model, const param_t& pa
   const float g = param.gravity;          // Specific Gravity [m/s^2]
   const float nu = param.viscosity;       // Kinematic Viscosity [m^2/s]
 
-  // Surface Normal Vector
+  // Initial Velocity Estimate
   const vec3 normal = __normal(model, part.pos, scale);
   const vec2 average_speed = __avespeed(model, part);
-
-  // Initial Velocity Estimate
   part.speed = g * vec2(normal.x, normal.y) + nu * average_speed;
   if(glm::length(part.speed) == 0.0f)
     return;
 
-  part.ds = glm::length(cl) / glm::length(part.speed);
-  part.npos = part.pos + part.ds*(part.speed / cl);
+  // Initial Tracking Values
   part.dspeed = part.speed;
-
-  part.vol = Ac * R; // [m^3/y]
+  part.vol = Ac * R;          // [m^3/y]
   part.sed = 0.0f;
 
+  // Initial Trajectory
+  part.ds = glm::length(cl) / glm::length(part.speed);
+  part.npos = part.pos + part.ds*(part.speed / cl);
+  
 }
 
 //! Note: Return's false if we wish to terminate!
@@ -134,68 +157,47 @@ __device__ void __track(model_t& model, const particle_t& part, const size_t N){
 
 }
 
+//! Implicit Euler Forward Integration for Gravity
+//! 
+//! Shear-Stress and Viscosity Terms:
+//!  The viscosity and shear-stress are some combination
+//!  of bed shear-stress from friction, and mixing with the
+//!  bulk stream from viscosity. These two terms are linearly
+//!  related to the velocity and bulk velocity deviation
+//!  by some constants k1 and k2 respectively.
+//!
+//!  The only key question is how these parameters scale with
+//!  the space and time resolution of the simulation.
+//!
+//! Integrate Sub-Solution Quantities
+//!  Note: Integrated in Quasi-Static Time
+//!    using an Implicit Forward Scheme
+//!
 __device__ bool __integrate(const model_t& model, particle_t& part, const param_t& param){
 
   const vec3 scale = model.scale * 1E3f;  // Cell Scale [m] (conv. from km)
   const vec2 cl = vec2(scale.x, scale.y); // Cell Length [m, m]
   const float g = param.gravity;          // Specific Gravity [m/s^2]
-
-  //
-  // Integrate Sub-Solution Quantities
-  //  Note: Integrated in Quasi-Static Time
-  //    using an Implicit Forward Scheme
-
-  part.vol = 1.0f/(1.0f + part.ds*param.evapRate)*part.vol;
-
-  // Implicit Euler Forward Integration for Gravity
-  // 
-  // Shear-Stress and Viscosity Terms:
-  //  The viscosity and shear-stress are some combination
-  //  of bed shear-stress from friction, and mixing with the
-  //  bulk stream from viscosity. These two terms are linearly
-  //  related to the velocity and bulk velocity deviation
-  //  by some constants k1 and k2 respectively.
-  //
-  //  The only key question is how these parameters scale with
-  //  the space and time resolution of the simulation.
-  
-  const float k1 = param.bedShear;
-  const float k2 = param.viscosity;
+  const float k1 = param.bedShear;        // Shear-Stress Bed-Shear
+  const float k2 = param.viscosity;       // Shear-Stress Viscosity
 
   const vec3 normal = __normal(model, part.pos, scale);
   const vec2 average_speed = __avespeed(model, part);
-
+  
   part.speed = part.speed + part.ds * g * vec2(normal.x, normal.y);
-
+  
   part.speed =  1.0f/(1.0f + part.ds * (k1+k2))*part.speed + part.ds*k2/(1.0f + part.ds*(k1+k2))*average_speed;
   part.dspeed = 1.0f/(1.0f + part.ds * (k1+k2))*part.dspeed;
-
+  
   if(glm::length(part.speed) == 0.0f)
-    return false;
+  return false;
+  
+  part.vol = 1.0f/(1.0f + part.ds*param.evapRate)*part.vol;
 
   part.ds = glm::length(cl)/glm::length(part.speed);
   part.npos = part.pos + part.ds * (part.speed / cl);
 
   return true;
-
-}
-
-__device__ float __slope(const model_t& model, const particle_t& part, const param_t& param){
-
-  const vec3 scale = model.scale * 1E3f;  // Cell Scale [m] (conv. from km)
-  const vec2 cl = vec2(scale.x, scale.y); // Cell Length [m, m]
-
-  float slope = -param.exitSlope;
-  float h0 = (model.height[part.ind] + model.sediment[part.ind])*scale.z;
-  float h1 = h0 + slope * glm::length(cl);
-  
-  if(!model.index.oob(part.npos)){
-    const int nind = model.index.flatten(part.npos);
-    h1 = (model.height[nind] + model.sediment[nind])*scale.z;
-    slope = (h1 - h0)/glm::length(cl);
-  }
-
-  return slope;
 
 }
 
@@ -340,6 +342,23 @@ __global__ void solve(model_t model, const size_t N, const param_t param){
     if(!__integrate(model, part, param))
       break;
 
+    /*
+    // alternative order:
+    // Shift Trajectory
+    if(!__move(model, part))
+      break;
+  
+    // Integrate Differential Quantities
+    if(!__integrate(model, part, param))
+      break;
+
+    // Accumulate Estimated Quantities
+    __track(model, part, N);
+    
+    // Mass-Transfer
+    __masstransfer2(model, part, param, N);
+    */
+
   }
 
 }
@@ -395,6 +414,7 @@ void erode(model_t& model, const param_t param, const size_t steps){
 
     set(model.discharge_track, 0.0f);
     set(model.momentum_track, vec2(0.0f));
+    cudaDeviceSynchronize();
 
     solve<<<block(n_samples, 512), 512>>>(model, n_samples, param);
     cudaDeviceSynchronize();
