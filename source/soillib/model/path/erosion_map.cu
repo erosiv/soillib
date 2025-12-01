2e296ba250e14863ec549e7f7537f6873b7b99e9
Nicholas McDonald <nicholasmcdonald40@gmail.com>
2025-12-01 17:51:31 +0100

    slight improvement to the gradient function

--- source/soillib/model/path/erosion_map.cu
+++ source/soillib/model/path/erosion_map.cu
@@ -8,44 +8,66 @@
 
 #include <soillib/model/path/erosion.hpp>
 
+//! Various Method of Computing Gradients:
+//! In general, we want it to vary smoothly,
+//! so it is most likely useful if we interpolate
+//! somehow. We can either do that using a gradient
+//! method and then lerping, or using the analytical
+//! gradients of a higher order method.
+
+__device__ silt::vec2 __glocal (
+  const silt::tensor_t<float>& height,
+  const silt::shape shape,
+  const silt::vec3 scale,
+  const silt::ivec2 ipos
+) {
+
+  const int i00 = shape.flatten(ipos + silt::ivec2( 0, 0));
+  const int in0 = shape.flatten(ipos + silt::ivec2(-1, 0));
+  const int ip0 = shape.flatten(ipos + silt::ivec2( 1, 0));
+  const int i0n = shape.flatten(ipos + silt::ivec2( 0,-1));
+  const int i0p = shape.flatten(ipos + silt::ivec2( 0, 1));
+
+  const float h = height[i00];
+  const float hn0 = shape.oob(ipos + silt::ivec2(-1, 0)) ? CUDART_NAN_F : height[in0];
+  const float hp0 = shape.oob(ipos + silt::ivec2( 1, 0)) ? CUDART_NAN_F : height[ip0];
+  const float h0n = shape.oob(ipos + silt::ivec2( 0,-1)) ? CUDART_NAN_F : height[i0n];
+  const float h0p = shape.oob(ipos + silt::ivec2( 0, 1)) ? CUDART_NAN_F : height[i0p];
+
+  const float gxn = (h - hn0) * scale.z / scale.x;
+  const float gxp = (hp0 - h) * scale.z / scale.x;
+  const float gyn = (h - h0n) * scale.z / scale.y;
+  const float gyp = (h0p - h) * scale.z / scale.y;
+
+  float gx = 0.0f;
+  float gy = 0.0f;
+
+  if(!__isnanf(gxn) && gxn > 0 && abs(gxn) > abs(gx)) {
+    gx = gxn;
+  }
+  if(!__isnanf(gxp) && gxp < 0 && abs(gxp) > abs(gx)) {
+    gx = gxp;
+  }
+
+  if(!__isnanf(gyn) && gyn > 0 && abs(gyn) > abs(gy)) {
+    gy = gyn;
+  }
+  if(!__isnanf(gyp) && gyp < 0 && abs(gyp) > abs(gy)) {
+    gy = gyp;
+  }
+
+  return silt::vec2(gx, gy);
+
+}
+
 __device__ silt::vec2 __grad(
   const silt::tensor_t<float>& height,
   const silt::shape shape,
   const silt::vec3 scale,
   const silt::vec2 pos
-){
+) {
 
-  const int i00 = shape.flatten(pos + silt::vec2( 0, 0));
-  const int in0 = shape.flatten(pos + silt::vec2(-1, 0));
-  const int ip0 = shape.flatten(pos + silt::vec2( 1, 0));
-  const int i0n = shape.flatten(pos + silt::vec2( 0,-1));
-  const int i0p = shape.flatten(pos + silt::vec2( 0, 1));
-
-  const float h = height[i00] * scale.z;
-  const float hn0 = shape.oob(pos + silt::vec2(-1, 0)) ? h : height[in0] * scale.z;
-  const float hp0 = shape.oob(pos + silt::vec2( 1, 0)) ? h : height[ip0] * scale.z;
-  const float h0n = shape.oob(pos + silt::vec2( 0,-1)) ? h : height[i0n] * scale.z;
-  const float h0p = shape.oob(pos + silt::vec2( 0, 1)) ? h : height[i0p] * scale.z;
-
-  float gx = 0.0f;
-  if(__isnanf(hn0)) gx = (hp0 - h)/scale.x;
-  if(__isnanf(hp0)) gx = (h - hn0)/scale.x;
-  if(!__isnanf(hp0) && !__isnanf(hn0)){
-    if(hn0 < hp0) gx = (h - hn0)/scale.x;
-    if(hp0 < hn0) gx = (hp0 - h)/scale.x;
-    // if they are the same, slope is zero
-  }
-
-  float gy = 0.0f;
-  if(__isnanf(h0n)) gy = (h0p - h)/scale.y;
-  if(__isnanf(h0p)) gy = (h - h0n)/scale.y;
-  if(!__isnanf(h0p) && !__isnanf(h0n)){
-    if(h0n < h0p) gy = (h - h0n)/scale.y;
-    if(h0p < h0n) gy = (h0p - h)/scale.y;
-    // if they are the same, slope is zero
-  }
-
-  return silt::vec2(gx, gy);
+  return __glocal(height, shape, scale, pos);
 
 }
 
