a37a51f7510cddfefeefaf18d35d6fa9fe7f45a0
Nicholas McDonald <nicholasmcdonald40@gmail.com>
2025-12-01 19:50:00 +0100

    outlined the replacement of slope with interpolated slope

--- source/soillib/model/path/erosion_map.cu
+++ source/soillib/model/path/erosion_map.cu
@@ -60,6 +60,49 @@ __device__ silt::vec2 __glocal (
 
 }
 
+__device__ float __slocal (
+  const silt::tensor_t<float>& height,
+  const silt::shape shape,
+  const silt::vec3 scale,
+  const silt::ivec2 ipos,
+  const float exitSlope
+){
+
+  const int i00 = shape.flatten(ipos + silt::ivec2( 0, 0));
+  const int in0 = shape.flatten(ipos + silt::ivec2(-1, 0));
+  const int ip0 = shape.flatten(ipos + silt::ivec2( 1, 0));
+  const int i0n = shape.flatten(ipos + silt::ivec2( 0,-1));
+  const int i0p = shape.flatten(ipos + silt::ivec2( 0, 1));
+
+  const float h = height[i00];
+  const float hn0 = shape.oob(ipos + silt::ivec2(-1, 0)) ? CUDART_NAN_F : height[in0];
+  const float hp0 = shape.oob(ipos + silt::ivec2( 1, 0)) ? CUDART_NAN_F : height[ip0];
+  const float h0n = shape.oob(ipos + silt::ivec2( 0,-1)) ? CUDART_NAN_F : height[i0n];
+  const float h0p = shape.oob(ipos + silt::ivec2( 0, 1)) ? CUDART_NAN_F : height[i0p];
+
+  // Min Gradient Computation w. Bounds Handling
+  const float gxn = (h - hn0) * scale.z / scale.x;
+  const float gxp = (h - hp0) * scale.z / scale.x;
+  const float gyn = (h - h0n) * scale.z / scale.y;
+  const float gyp = (h - h0p) * scale.z / scale.y;
+  
+  float gx = 0.0f;
+  if(!__isnanf(gxn)) gx = glm::max(gx, gxn);
+  if(!__isnanf(gxp)) gx = glm::max(gx, gxp);
+  if(__isnanf(gxn) || __isnanf(gxp))
+    gx = glm::max(gx, exitSlope);
+
+  float gy = 0.0f;
+  if(!__isnanf(gyn)) gy = glm::max(gy, gyn);
+  if(!__isnanf(gyp)) gy = glm::max(gy, gyp);
+  if(__isnanf(gyn) || __isnanf(gyp))
+    gy = glm::max(gy, exitSlope);
+
+  // Write to 2D vector view
+  return glm::length(silt::vec2(gx, gy));
+
+}
+
 __device__ silt::vec2 __grad(
   const silt::tensor_t<float>& height,
   const silt::shape shape,
@@ -67,7 +110,12 @@ __device__ silt::vec2 __grad(
   const silt::vec2 pos
 ) {
 
-  return __glocal(height, shape, scale, pos);
+  const silt::vec2 w = pos - glm::floor(pos);
+  const silt::vec2 g00 = __glocal(height, shape, scale, silt::ivec2(pos) + silt::ivec2(0, 0));
+  const silt::vec2 g01 = __glocal(height, shape, scale, silt::ivec2(pos) + silt::ivec2(0, 1));
+  const silt::vec2 g10 = __glocal(height, shape, scale, silt::ivec2(pos) + silt::ivec2(1, 0));
+  const silt::vec2 g11 = __glocal(height, shape, scale, silt::ivec2(pos) + silt::ivec2(1, 1));
+  return g00 * (1.0f - w.x) * (1.0f - w.y) + g01 * (1.0f - w.x) * w.y + g10 * w.x * (1.0f - w.y) + g11 * w.x * w.y;
 
 }
 
@@ -79,33 +127,14 @@ __device__ float __slope (
   const float exitSlope
 ){
 
-  const int i00 = shape.flatten(pos + silt::vec2( 0, 0));
-  const int in0 = shape.flatten(pos + silt::vec2(-1, 0));
-  const int ip0 = shape.flatten(pos + silt::vec2( 1, 0));
-  const int i0n = shape.flatten(pos + silt::vec2( 0,-1));
-  const int i0p = shape.flatten(pos + silt::vec2( 0, 1));
+//  const silt::vec2 w = pos - glm::floor(pos);
+  const float s00 = __slocal(height, shape, scale, silt::ivec2(pos) + silt::ivec2(0, 0), exitSlope);
+  return s00;
 
-  const float h = height[i00] * scale.z;
-  const float hn0 = shape.oob(pos + silt::vec2(-1, 0)) ? CUDART_NAN_F : height[in0] * scale.z;
-  const float hp0 = shape.oob(pos + silt::vec2( 1, 0)) ? CUDART_NAN_F : height[ip0] * scale.z;
-  const float h0n = shape.oob(pos + silt::vec2( 0,-1)) ? CUDART_NAN_F : height[i0n] * scale.z;
-  const float h0p = shape.oob(pos + silt::vec2( 0, 1)) ? CUDART_NAN_F : height[i0p] * scale.z;
-
-  // Min Gradient Computation w. Bounds Handling
-  float gx = 0.0f;
-  if(!__isnanf(hn0)) gx = glm::max(gx, (h - hn0)/scale.x);
-  if(!__isnanf(hp0)) gx = glm::max(gx, (h - hp0)/scale.x);
-  if(__isnanf(hn0) || __isnanf(hp0))
-    gx = glm::max(gx, exitSlope);   
-
-  float gy = 0.0f;
-  if(!__isnanf(h0n)) gy = glm::max(gy, (h - h0n)/scale.y);
-  if(!__isnanf(h0p)) gy = glm::max(gy, (h - h0p)/scale.y);
-  if(__isnanf(h0n) || __isnanf(h0p))
-    gy = glm::max(gy, exitSlope);
-
-  // Write to 2D vector view
-  return glm::length(silt::vec2(gx, gy));
+  // const float s01 = __slocal(height, shape, scale, silt::ivec2(pos) + silt::ivec2(0, 1), exitSlope);
+  // const float s10 = __slocal(height, shape, scale, silt::ivec2(pos) + silt::ivec2(1, 0), exitSlope);
+  // const float s11 = __slocal(height, shape, scale, silt::ivec2(pos) + silt::ivec2(1, 1), exitSlope);
+  // return s00 * (1.0f - w.x) * (1.0f - w.y) + s01 * (1.0f - w.x) * w.y + s10 * w.x * (1.0f - w.y) + s11 * w.x * w.y;
 
 }
 
