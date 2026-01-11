f6b3b8f54a4d995ebfc4d4aa052f2517c82bde6c
Nicholas McDonald <nicholasmcdonald40@gmail.com>
2026-01-11 15:04:54 +0100

    revalidated the dimensionalization and added explicit notation for the dimensionality of the transport problem.

--- source/soillib/model/path/path.cu
+++ source/soillib/model/path/path.cu
@@ -48,9 +48,9 @@ __device__ float __stepsize (
 
 }
 
-template<size_t D>
+template<size_t K>                      //!< K = Transport Tensor Components
 __global__ void __solve_uniform (
-  silt::tensor_t<float> flux,           //!< Flux Integral Estimate [X/s]
+  silt::tensor_t<float> flux,           //!< Flux Integral Estimate [X*m^D/s]
   const silt::tensor_t<float> flow,     //!< Flow-Field Tensor      [m/s]
   const silt::tensor_t<float> source,   //!< Source Term Tensor     [X/s]
   const silt::tensor_t<float> decay,    //!< Decay Term Tensor      [1/s]
@@ -67,24 +67,27 @@ __global__ void __solve_uniform (
   if(n >= rng.elem()) return;
 
   // Extract Correct Dimension Views
-  using vecD = silt::fvec<D>;               //!< Vector of Dimension
-  auto fluxView = flux.view<vecD>();        //!< Tensorised Flux View
-  auto sourceView = source.view<vecD>();    //!< Tensorised Source View
-  auto flowView = flow.view<silt::vec2>();  //!< Tensorised Flow View
+  constexpr size_t D = 2;                   //!< Dimensionality of Domain
+  using vecD = silt::fvec<D>;               //!< Vector of D Components
+  using vecK = silt::fvec<K>;               //!< Vector of K Components
+  auto fluxView = flux.view<vecK>();        //!< Tensorised Flux View         [X/s]
+  auto flowView = flow.view<vecD>();        //!< Tensorised Flow View         [m/s]
+  auto sourceView = source.view<vecK>();    //!< Tensorised Source View       [X/s]
 
   // Initialize Particle State
-  float att = 1.0f;                         //!< Cumulative Flux Attenuation
-  float lambda = 0.0f;                      //!< Cumulative Time Travelled
-  silt::vec2 pos = silt::vec2 {             //!< Position in Grid-Space
+  float att = 1.0f;                         //!< Cumulative Flux Attenuation  [1]
+  float lambda = 0.0f;                      //!< Cumulative Time Travelled    [s]
+  vecD pos = vecD {                         //!< Position in Grid-Space       [1]
     curand_uniform(&rng[n])*float(shape[0]),
     curand_uniform(&rng[n])*float(shape[1])
   };
   int ind = shape.flatten(pos);             //!< Grid Tensor Index
 
   // Upstream Contributions Sample
-  const float P = 1.0f / float(shape.elem);
-  const vecD S = sourceView[ind] / P;
-  if(glm::length(S) < epsilon)
+  const float A = (scale.x * scale.y);      //!< Area of Cell           [m^D]
+  const float P = 1.0f / (A * shape.elem);  //!< Probability of Sample  [1/m^D]
+  const vecK S = sourceView[ind] / P;       //!< Sampled Source Rate    [X*m^D/s]
+  if(glm::length(S) < epsilon)              //!< Early Termination
     return;
 
   // the flow evolution rule?
@@ -92,67 +95,76 @@ __global__ void __solve_uniform (
   //  don't really go away. We rely on the well-structuredness of
   //  the velocity field, which we cannot always do.
   //silt::vec2 v = flowView[ind];
-  const sample_t<silt::vec2, 2, 1> v_support = sample_t<silt::vec2, 2, 1>::gather(flowView, silt::ivec2(shape[0], shape[1]), pos);
-  silt::vec2 v = v_support.val();
+  const sample_t<vecD, D, 1> v_support = sample_t<vecD, D, 1>::gather(flowView, vecD(shape[0], shape[1]), pos);
+  vecD v = v_support.val();
 
   // Integrate along Streamline
   int step = 0;
   while(!shape.oob(pos) && epsilon < abs(att) && ++step < maxstep) {
 
-    // Accumulate Estimate
-    const int nind = shape.flatten(pos);  // New Index?
+    // Accumulate Estimate upon exiting Cell
+    const int nind = shape.flatten(pos);
     if(nind != ind) {
       ind = nind;
-      if constexpr(D >= 1){
+      if constexpr(K >= 1){
         atomicAdd(&fluxView[ind].x, S.x * att);
       }
-      if constexpr(D >= 2){
+      if constexpr(K >= 2){
         atomicAdd(&fluxView[ind].y, S.y * att);
       }
     }
 
-    const sample_t<silt::vec2, 2, 1> v_support = sample_t<silt::vec2, 2, 1>::gather(flowView, silt::ivec2(shape[0], shape[1]), pos);
+    // Sample Velocity Field
+    const sample_t<vecD, D, 1> v_support = sample_t<vecD, D, 1>::gather(flowView, vecD(shape[0], shape[1]), pos);
     v = v_support.val();
 //    v = flowView[ind];
 
-    const float v_len = glm::length(v);
+    const float v_len = glm::length(v);         //!< Velocity [m/s]
     if(v_len < epsilon)
       break;
 
     // Update the Position in Grid-Space
-    const silt::vec2 v_norm = v / v_len;        // Normalized Direction
-    const float step = __stepsize(pos, v_norm); // Step-Size in Gridspace
-    pos += step * v_norm;                       // Grid Position Update
+    const silt::vec2 v_norm = v / v_len;        //!< Normalized Direction   [1]
+    const float step = __stepsize(pos, v_norm); //!< Step-Size in Gridspace [1]
+    pos += step * v_norm;                       //!< Grid Position Update   [1]
 
     // Update the Real Time and Attenuation
-    const float dlambda = glm::length(step * scale) / v_len;  // Real Time Travelled
-    att *= __expf(-dlambda * decay[ind]);                     // Decay Attenuation
-    lambda += dlambda;                                        // Update Real Time
+    const float dlambda = glm::length(step * scale) / v_len;  //!< Real Time Travelled  [t]
+    att *= __expf(-dlambda * decay[ind]);                     //!< Decay Attenuation    [1]
+    lambda += dlambda;                                        //!< Update Real Time     [t]
 
   }
 
 }
 
-template<size_t D>
+template<size_t K>                      //!< K = Transport Tensor Components
 __global__ void __normalize (
-  silt::tensor_t<float> flux,           //!< Flux Integral Estimate []
-  const silt::tensor_t<float> flow,     //!< Flow-Field Tensor      []
-  const silt::tensor_t<float> source,   //!< Source Term 
-  const silt::vec2 scale,               //!< Cell Scale
-  const size_t count                    //!< Sample Count
+  silt::tensor_t<float> flux,           //!< Flux Integral Estimate [X*m^D/s] -> [X]
+  const silt::tensor_t<float> flow,     //!< Flow-Field Tensor      [m/s]
+  const silt::tensor_t<float> source,   //!< Source Term            [X/s]
+  const silt::vec2 scale,               //!< Cell Scale             [m]
+  const size_t count                    //!< Sample Count           [1]
 ) {
 
   const unsigned int n = blockIdx.x * blockDim.x + threadIdx.x;
-  if(n >= flux.elem()/D) return;
+  if(n >= flux.elem()/K) return;
 
-  using vecD = silt::fvec<D>;               //!< Vector of Dimension
-  auto fluxView = flux.view<vecD>();        //!< Tensorised Flux View
-  auto sourceView = source.view<vecD>();    //!< Tensorised Source View
-  auto flowView = flow.view<silt::vec2>();  //!< Tensorised Flow View
+  constexpr size_t D = 2;                 //!< Dimensionality of Domain
+  using vecD = silt::fvec<D>;             //!< Vector of D Components
+  using vecK = silt::fvec<K>;             //!< Vector of K Components
+  auto fluxView = flux.view<vecK>();      //!< Tensorised Flux View   [X*m^D/s]
+  auto flowView = flow.view<vecD>();      //!< Tensorised Flow View   [m/s]
+  auto sourceView = source.view<vecK>();  //!< Tensorised Source View [X/s]
 
-  const silt::vec2 v = flowView[n];
-  const float norm = abs(v.x * scale.y) + abs(v.y * scale.x);
-  fluxView[n] = sourceView[n] + fluxView[n] / float(count) / norm;
+  const vecD v = flowView[n];                                             //!< [m/s]
+  const float A = scale.x * scale.y;                                      //!< [m^D]
+  const float norm = abs(v.x * scale.y) + abs(v.y * scale.x);             //!< [m^D/s]
+
+  // Note: We add the scaled source term here, so that even positions with zero samples
+  //  have a positive value corresponding to their local source term. This additional
+  //  value is NOT divided by the sample count. Everything is normalized together.
+
+  fluxView[n] = (sourceView[n] * A + fluxView[n] / float(count)) / norm;  //!< [X]
 
 }
 
@@ -165,12 +177,12 @@ namespace soil {
 //! of the source tensor is the dimensionality of the transported quantity.
 //!
 silt::tensor solve_uniform (
-  const silt::tensor_t<float> flow,     //!< Flow-Field Tensor      [m/s x 2]
-  const silt::tensor_t<float> source,   //!< Source Term Tensor     [X/s x D]
-  const silt::tensor_t<float> decay,    //!< Decay Term Tensor      [1/s x 1]
-  silt::tensor_t<silt::rng> rng,        //!< Random Number Source   []
-  const silt::vec2 scale,               //!< Cell Scale             [m x 2]
-  const size_t count                    //!< Sample Count           []
+  const silt::tensor_t<float> flow,     //!< Flow-Field Tensor      [m/s]
+  const silt::tensor_t<float> source,   //!< Source Term Tensor     [X/s]
+  const silt::tensor_t<float> decay,    //!< Decay Term Tensor      [1/s]
+  silt::tensor_t<silt::rng> rng,        //!< Random Number Source   [1]
+  const silt::vec2 scale,               //!< Cell Scale             [m]
+  const size_t count                    //!< Sample Count           [1]
 ) {
 
   // The Accumulated Flux has the same Dimension as the Source-Term.
